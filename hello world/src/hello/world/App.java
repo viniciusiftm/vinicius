22f5d300e316988dcc69ac00cc83fd6de2d89701
Vinicius <Vinicius@vinicius-pc>
2015-05-28 21:19:36 -0300

    agora vai

--- hello world/src/hello/world/App.java
+++ hello world/src/hello/world/App.java
@@ -3,7 +3,7 @@ package hello.world;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("veja o git no eclipse com Egit!!!?");
+		System.out.println("veja o git no eclipse com Egit!!!");
 
 	}
 
